63b1f97ba3402810a351c6ffda859b1a1b40f47d
Zvone Gazvoda <zvone.gazvoda@cloud.si>
2017-09-11 10:30:09 +0200

    Added Javadoc.

--- src/main/java/com/kumuluz/ee/cors/utils/ServletCrossOriginAnnotationProcessorUtil.java
+++ src/main/java/com/kumuluz/ee/cors/utils/ServletCrossOriginAnnotationProcessorUtil.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.kumuluz.ee.cors.utils;
 
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
@@ -15,7 +35,10 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 /**
- * Created by zvoneg on 27/07/17.
+ * ServletCrossOriginAnnotationProcessorUtil class.
+ *
+ * @author Zvone Gazvoda
+ * @since 1.0.0
  */
 public class ServletCrossOriginAnnotationProcessorUtil implements CrossOriginAnnotationProcessorUtil {
 
